ab821c24c5a514a021c8f44a7384b090cd2eb0b3
markodjordjic <marko.d@prodactivesolutions.com>
2022-12-23 13:39:40 +0100

    - initial app created - categories blank screen added

---
+++ expense_planner/lib/categories_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
