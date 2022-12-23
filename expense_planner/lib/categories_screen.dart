6e8554dbddca3678455da61990c58f2d000da6f7
markodjordjic <marko.d@prodactivesolutions.com>
2022-12-23 16:02:09 +0100

    - category item added

--- expense_planner/lib/categories_screen.dart
+++ expense_planner/lib/categories_screen.dart
@@ -1,11 +1,17 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GridView(
+      children: [],
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 1.5,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
+    );
   }
 }
---
+++ expense_planner/lib/data/categories_data.dart
@@ -0,0 +1,7 @@
+import 'package:expense_planner/models/category.dart';
+
+const categoriesDummy = [
+  Category(id: "1", title: "Soccer"),
+  Category(id: "2", title: "Basketball"),
+  Category(id: "3", title: "Volleyball"),
+];
