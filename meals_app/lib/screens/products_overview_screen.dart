5877bb51a18a7152668f48e776a4574efe6d9a29
markodjordjic <marko.d@prodactivesolutions.com>
2023-02-21 16:43:48 +0100

    - product model added - products overview screen created

---
+++ meals_app/lib/screens/products_overview_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  const ProductsOverviewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
