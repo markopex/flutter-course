537e9e79c83cca9790b731dc50c149a455a6c57f
markodjordjic <marko.d@prodactivesolutions.com>
2023-02-27 21:25:55 +0100

    - product details screen implemented

--- meals_app/lib/screens/product_details_screen.dart
+++ meals_app/lib/screens/product_details_screen.dart
@@ -13,8 +13,42 @@ class ProductDetailsScreen extends StatelessWidget {
     final product =
         Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
-        appBar: AppBar(
-      title: Text(product.title),
-    ));
+      appBar: AppBar(
+        title: Text(product.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.fill,
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              "\$${product.price}",
+              style: const TextStyle(color: Colors.grey),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              width: double.infinity,
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
